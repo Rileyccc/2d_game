7d7515696addb8a9e80910e994f292dd67e082a5
rileyccc <rileyclark14@icloud.com>
2021-09-28 13:07:14 -0700

    add onload function

---
+++ javascript/game.js
@@ -0,0 +1,12 @@
+var gl;
+var points;
+
+var NumPoints = 5000;
+
+window.onload = function init()
+{
+    var canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+}
