e08e50e6bea793beacd46284b0292fe0805febce
rileyccc <rileyclark14@icloud.com>
2021-09-28 14:31:06 -0700

    load in shaders

--- javascript/game.js
+++ javascript/game.js
@@ -5,8 +5,29 @@ var NumPoints = 5000;
 
 window.onload = function init()
 {
-    var canvas = document.getElementById( "gl-canvas" );
-    
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
+    var canvas = document.getElementById('canvas');
+    var g1 = canvas.getContext('webgl')
+    if ( !gl ) { 
+        alert( "WebGL isn't available");
+     	gl = canvas.getContext('experimental-webgl');
+    }
+    if (!gl){
+        alert('your browser does not support webgl');
+    }
+
+    canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	gl.viewport(0,0,canvas.width,canvas.height);
+
+
+    //////////////////////////////////
+	// create/compile/link shaders  //
+	//////////////////////////////////
+	program = initShadersFromFiles(gl, './shaders/vertex_shader.glsl','./shaders/fragement_shader.glsl');
+    g1.useProgram(program);
+
+    if(!gl.getProgramParameter(program, gl.LINK_STATUS)){
+        console.error('error linking the program', gl.getProgramInfo(program) );
+        return;
+    }
 }
